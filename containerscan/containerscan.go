f9cfd4ac609bef2d252f1ab5f79bb9e51ebed60a
Doug Edgar <dedgar@redhat.com>
2019-07-29 08:36:20 -0800

    testing docker scanning 1

--- containerscan/containerscan.go
+++ containerscan/containerscan.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/rhdedgar/pleg-watcher/channels"
 	clscmd "github.com/rhdedgar/pleg-watcher/cmd"
+	"github.com/rhdedgar/pleg-watcher/docker"
 	"github.com/rhdedgar/pleg-watcher/models"
 	"github.com/rhdedgar/pleg-watcher/runcspec"
 	mainscan "github.com/rhdedgar/pleg-watcher/scanner"
@@ -42,7 +43,7 @@ func custSplit(s, d string, i int) string {
 
 // custReg takes 2 arguments and returns a string slice.
 //
-// scanOut is the string output from the crio /proc/$PID/mountinfo file.
+// scanOut is the string output from the container /proc/$PID/mountinfo file.
 //
 // regString is the `raw string` containing the regex match pattern to use.
 func custReg(scanOut, regString string) []string {
@@ -176,7 +177,6 @@ func PrepCrioScan(cCon models.Status) {
 	cID := cCon.Status.ID
 
 	cLayers, err := getCrioLayers(cID)
-	//rootFS, err := getRootFS(cID)
 
 	if err != nil {
 		fmt.Println(err)
@@ -198,12 +198,7 @@ func PrepCrioScan(cCon models.Status) {
 	scannerOptions.ScanResultsDir = scanResultsDir
 	scannerOptions.PostResultURL = postResultURL
 	scannerOptions.OutFile = outFile
-
-	//fmt.Printf("Scanning both %v and %v \n", rootFS, "/host"+rootFS)
-
-	//for _, scandir := range []string{rootFS, "/host" + rootFS} {
-
-	scannerOptions.ScanDir = scanDir //filepath.Dir(rootFS)
+	scannerOptions.ScanDir = scanDir
 
 	if err := scannerOptions.Validate(); err != nil {
 		fmt.Println("Error validating scanner options: ", err)
@@ -212,6 +207,105 @@ func PrepCrioScan(cCon models.Status) {
 	scanner := mainscan.NewDefaultContainerLayerScanner(*scannerOptions)
 	scanner.ScanOutputs.ScanResults.NameSpace = cCon.Status.Labels.IoKubernetesPodNamespace
 	scanner.ScanOutputs.ScanResults.PodName = cCon.Status.Labels.IoKubernetesPodName
+
+	if err := scanner.AcquireAndScan(); err != nil {
+		fmt.Println("Error returned from scanner: ", err)
+	}
+
+	err = unix.Unmount(scanDir, 0)
+	if err != nil {
+		fmt.Println("Error unmounting scanDir after scanning: ", err)
+	}
+
+	os.Remove(scanDir)
+	if err != nil {
+		fmt.Println("Error removing scanDir after unmounting: ", err)
+	}
+}
+
+// getDockerLayers takes a containerID string and queries docker inspect
+// for overlayFS mount data found in /proc/PID/mountinfo.
+func getDockerLayers(containerID string, procID int) ([]string, error) {
+	var layers []string
+	var dockerLayers []string
+
+	fmt.Println("Getting docker layers: ", containerID)
+
+	mountPath := "/proc/" + strconv.Itoa(procID) + "/mountinfo"
+
+	f, err := os.Open(mountPath)
+	if err != nil {
+		fmt.Println("Error opening file, waiting 5 seconds in case it just hasn't been created yet: ", mountPath, err)
+		time.Sleep(5 * time.Second)
+		f, err = os.Open(mountPath)
+	}
+
+	defer f.Close()
+
+	bufScan := bufio.NewScanner(f)
+	bufScan.Scan()
+	scanOut := bufScan.Text()
+
+	if err := bufScan.Err(); err != nil {
+		return dockerLayers, fmt.Errorf("Error reading layer %v", err)
+	}
+
+	layers = append(layers, custReg(scanOut, `lowerdir=(.*),upperdir`)...)
+
+	for _, l := range layers {
+		items := strings.Split(l, ":")
+		for _, i := range items {
+			j := custSplit(i, ",", 0)
+			j = custSplit(j, "=", 1)
+
+			dockerLayers = append(dockerLayers, j)
+		}
+	}
+	return dockerLayers, nil
+}
+
+// PrepDockerScan gets a slice of container filesystem layers from getDockerLayers
+// and then initiates a scan for each of the returned layers.
+func PrepDockerScan(dCon docker.DockerContainer) {
+	fmt.Printf("In scan block for %v in %v",
+		dCon[0].Config.Labels.IoKubernetesPodName,
+		dCon[0].Config.Labels.IoKubernetesPodNamespace)
+
+	scannerOptions := clscmd.NewDefaultContainerLayerScannerOptions()
+	cID := dCon[0].ID
+	pID := dCon[0].State.Pid
+
+	cLayers, err := getDockerLayers(cID, pID)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if len(cLayers) == 0 {
+		fmt.Println("layers returned empty")
+		return
+	}
+	fmt.Println(cLayers)
+
+	scanDir, err := mountOverlayFS(cLayers, cID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	scannerOptions.ScanResultsDir = scanResultsDir
+	scannerOptions.PostResultURL = postResultURL
+	scannerOptions.OutFile = outFile
+	scannerOptions.ScanDir = scanDir
+
+	if err := scannerOptions.Validate(); err != nil {
+		fmt.Println("Error validating scanner options: ", err)
+	}
+
+	scanner := mainscan.NewDefaultContainerLayerScanner(*scannerOptions)
+	scanner.ScanOutputs.ScanResults.NameSpace = dCon[0].Config.Labels.IoKubernetesPodNamespace
+	scanner.ScanOutputs.ScanResults.PodName = dCon[0].Config.Labels.IoKubernetesPodName
 
 	if err := scanner.AcquireAndScan(); err != nil {
 		fmt.Println("Error returned from scanner: ", err)
