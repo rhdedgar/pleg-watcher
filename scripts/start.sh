c1893b2f3cb27b6589bbac5d78318c7d1c3795f1
Doug Edgar <dedgar@redhat.com>
2020-01-23 15:50:42 -0800

    adding crio and docker mount options back

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,6 +12,19 @@ if [ "$OO_PAUSE_ON_START" = "true" ] ; then
   done
 fi
 
+
+echo "Creating mount points for crio and docker sockets and dependencies."
+touch /var/run/docker.sock
+touch /var/run/crio/crio.sock
+touch /usr/bin/docker-current
+touch /etc/sysconfig/docker
+
+echo "Mounting crio and docker dependencies."
+mount --bind -o ro /host/var/run/docker.sock /var/run/docker.sock
+mount --bind -o ro /host/var/run/crio/crio.sock /var/run/crio/crio.sock
+mount --bind -o ro /host/usr/bin/docker-current /usr/bin/docker-current
+mount --bind -o ro /host/etc/sysconfig/docker /etc/sysconfig/docker
+
 echo This container hosts the following applications:
 echo
 echo '/usr/bin/pleg-watcher'
