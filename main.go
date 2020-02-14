6f66b4e35802d291e5b21feb22dfbe45234b58cf
Doug Edgar <dedgar@redhat.com>
2020-02-13 21:42:59 -0800

    change ver num

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	var line models.LineInfo
 
-	fmt.Println("pleg-watcher v0.0.20, v0.0.17 was successful. testing SD_JOURNAL_FIELD_SYSTEMD_UNIT kubelet.go filtering now")
+	fmt.Println("pleg-watcher v0.0.21, v0.0.17 was successful. testing SD_JOURNAL_FIELD_SYSTEMD_UNIT kubelet.go filtering now")
 	line = make(chan string)
 
 	// This gets set up first so that chroot doesn't interfere with libraries loading.
